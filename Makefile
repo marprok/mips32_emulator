e6f093da10b714c4e3710beab328b18a33d88800
marprok <mariosprokopakis@gmail.com>
2019-09-03 12:59:23 +0300

    The "assembler" reads a file with assembly instructions in the form of inst, $reg1, $reg2, $reg3 and outputs a file with the binary representation of the instructions read.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-CFLAGS=-Wall -Wextra -pedantic -std=c99
+CFLAGS=-Wall -Wextra -pedantic -std=c99 -g
 
 main: main.o mips32.o
 	$(CC) -o main main.o mips32.o
@@ -7,5 +7,7 @@ main.o: main.c
 	$(CC) $(CFLAGS) -c main.c
 mips32.o: mips32.c mips32.h
 	$(CC) $(CFLAGS) -c mips32.c
+assembler: assembler.c
+	$(CC) $(CFLAGS) assembler.c -o assembler
 clean:
 	rm *.o main
--- assembler.c
+++ assembler.c
@@ -1,7 +1,10 @@
+#define _POSIX_C_SOURCE 200809L /*ssize_t and getline */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdint.h>
 
 static void trim(char** str)
 {
@@ -15,7 +18,7 @@ static void trim(char** str)
 	(*str)++;
 }
 
-int tokenize(char *str, char delim, char*** tokens)
+static int tokenize(char *str, char delim, char*** tokens)
 {
     int count = 1;
     int i = 0;
@@ -29,9 +32,18 @@ int tokenize(char *str, char delim, char*** tokens)
 	pointer++;
     }
     
-    if (tokens && *tokens)
+    if (tokens)
     {
+	if (*tokens)
+	{
+	    free(*tokens);
+	    printf("not NULL\n");
+	}
+	printf("NULL\n");
 	*tokens = (char**)malloc(count*sizeof(char*));
+    }else
+    {
+	return -1;
     }
     pointer = str;
     token = str;
@@ -53,20 +65,146 @@ int tokenize(char *str, char delim, char*** tokens)
     return count;
 }
 
+static int get_reg(const char* reg)
+{
+    return atoi(reg+2);
+}
+
+static char check_regs(char** regs, int count)
+{
+    int i = 0;
+    for (; i < count; ++i)
+    {
+	if (strncmp(regs[i], "$t", 2))
+	{
+	    fprintf(stderr, "Invalid Instruction(%s)!\n", regs[i]);
+	    return 0;
+	}
+    }
+    return 1;
+}
+
+static int32_t create_R(int8_t opcode, int8_t s, int8_t t,
+                        int8_t d, int8_t shamt, int8_t funct)
+{
+    int32_t inst = 0;
+    inst = opcode;
+
+    inst = inst << 5;
+    inst |= s;
+
+
+    inst = inst << 5;
+    inst |= t;
+
+
+    inst = inst << 5;
+    inst |= d;
+
+    // shamt
+    inst = inst << 5;
+    inst |= shamt;
+
+    // funct
+    inst = inst << 6;
+    inst |= funct;
+
+    return inst;
+}
+
+static int32_t encode(char** inst, int count)
+{
+    int32_t dinst = 0;
+    if (count != 4)
+    {
+	fprintf(stderr, "Invalid Instruction(%d)!\n",count);
+	exit(1);
+    }
+    if (!strcmp(inst[0], "add") || !strcmp(inst[0], "ADD"))
+    {
+	if (!check_regs(&inst[1], count-1))
+	    exit(1);
+	dinst = create_R(0x0, get_reg(inst[2]), get_reg(inst[3]), get_reg(inst[1]), 0x0, 0x20);
+    }else if (!strcmp(inst[0], "sub") || !strcmp(inst[0], "SUB"))
+    {
+	if (!check_regs(&inst[1], count-1))
+	    exit(1);
+	dinst = create_R(0x0, get_reg(inst[2]), get_reg(inst[3]), get_reg(inst[1]), 0x0, 0x22);
+    }else if (!strcmp(inst[0], "and") || !strcmp(inst[0], "AND"))
+    {
+	if (!check_regs(&inst[1], count-1))
+	    exit(1);
+	dinst = create_R(0x0, get_reg(inst[2]), get_reg(inst[3]), get_reg(inst[1]), 0x0, 0x24);
+    }else if (!strcmp(inst[0], "or") || !strcmp(inst[0], "OR"))
+    {
+	if (!check_regs(&inst[1], count-1))
+	    exit(1);
+	dinst = create_R(0x0, get_reg(inst[2]), get_reg(inst[3]), get_reg(inst[1]), 0x0, 0x25);
+    }else if (!strcmp(inst[0], "nor") || !strcmp(inst[0], "NOR"))
+    {
+	if (!check_regs(&inst[1], count-1))
+	    exit(1);
+	dinst = create_R(0x0, get_reg(inst[2]), get_reg(inst[3]), get_reg(inst[1]), 0x0, 0x25);
+    }else if (!strcmp(inst[0], "slt") || !strcmp(inst[0], "SLT"))
+    {
+	if (!check_regs(&inst[1], count-1))
+	    exit(1);
+	dinst = create_R(0x0, get_reg(inst[2]), get_reg(inst[3]), get_reg(inst[1]), 0x0, 0x25);
+    }
+    return dinst;
+}
+
 int main(int argc, char **argv)
 {
+    FILE* out = NULL;
+    FILE* in = NULL;
+    char ** tokens = NULL;
+    char *inst = NULL;
+    size_t len;
+    ssize_t nread;
+    int count;
+    int32_t result;
+    const char* bin = "bin.mips";
+
     if (argc != 2)
     {
-	printf("USAGE: ./assembler operation\n");
-	return 1;
-    }
-    char ** tokens;
-    int count = tokenize(argv[1], ',', &tokens);
-    for (int i = 0; i < count; ++i)
+	printf("./assembler file.mips\n");
+	exit(1);
+    }    
+
+    out = fopen("bin.mips", "wb");
+    if (!out)
     {
-	printf("token: %s\n", tokens[i]);
+	fprintf(stderr, "Cannot create/open file %s\n", bin);
+	exit(1);
     }
+
+    in = fopen(argv[1], "r");
+    if (!in)
+    {
+	fprintf(stderr, "Cannot open file %s\n", argv[1]);
+	exit(1);
+    }
+    
+    while ((nread = getline(&inst, &len, in)) > 0)
+    {
+	printf("line: %s", inst);
+	count = tokenize(inst, ',', &tokens);
+	if (count < 0)
+	{
+	    fprintf(stderr, "Error while tokenizing the command %s\n", inst);
+	    exit(1);
+	}
+	result = encode(tokens, count);
+	printf("0x%08X\n", result);
+	fwrite(&result, sizeof(result), 1, out);
+    }
+    /* Cleanup */
     if (tokens)
 	free(tokens);
+    if (inst)
+	free(inst);
+    fclose(out);
+    fclose(in);
     return 0;
 }
